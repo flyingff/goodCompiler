f003024062ef24b27e4c1f6c8150785e1f3d968a
wwwlxmgithub <mg@flyingff.net>
2015-07-11 20:46:46 +0800

    11

--- GoodCompiler/src/language/GrammaAnalyser.java
+++ GoodCompiler/src/language/GrammaAnalyser.java
@@ -13,16 +13,17 @@ import java.util.Properties;
 import java.util.Set;
 
 public class GrammaAnalyser {
+	private static final String EPSLON = "_", TERMINATOR = "#", START = "S";
 	private Properties p;
-	private Map<String, Set<Production>> map = new HashMap<String, Set<Production>>();	//左部相同的产生式集合
-	private List<Set<Item>> itemfamily = new ArrayList<>();								//项目集规范族
-	private Set<String> vset = new HashSet<>();											//所有符号的集合(包括终结符和非终结符)
-	private Set<Item> psset = new HashSet<Item>();										//所有项目的集合
-	private Set<String> vnset = new HashSet<String>();									//非终结符集合
-	private Map<String, Set<String>> firstSet = new HashMap<String, Set<String>>();		//所有非终结符的FIRST集合
-	private Map<String, Set<String>> lastSet = new HashMap<String, Set<String>>();		//所有非终结符的LAST集合
+	private Map<String, Set<Production>> map = new HashMap<String, Set<Production>>();	// 左部相同的产生式集合
+	private List<Set<Item>> itemfamily = new ArrayList<>();								// 项目集规范族
+	private Set<String> vset = new HashSet<>();											// 所有符号的集合(包括终结符和非终结符)
+	private Set<Item> psset = new HashSet<Item>();										// 所有项目的集合
+	private Set<String> vnset = new HashSet<String>();									// 非终结符集合
+	private Map<String, Set<String>> firstSet = new HashMap<String, Set<String>>();		// 所有非终结符的FIRST集合
+	private Map<String, Set<String>> lastSet = new HashMap<String, Set<String>>();		// 所有非终结符的LAST集合
 	
-	Map<Group, Integer> gotomap = new HashMap<Group, Integer>();						//GOTO转换表
+	Map<Group, Integer> gotomap = new HashMap<Group, Integer>();						// GOTO转换表
 	public GrammaAnalyser(InputStream is) {
 		p = new Properties();
 		try {
@@ -30,30 +31,30 @@ public class GrammaAnalyser {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		for(Entry<Object, Object> x : p.entrySet()){									//遍历属性表
+		for(Entry<Object, Object> x : p.entrySet()){									// 遍历属性表
 			Set<Production> xset = new HashSet<Production>();
-			String left = ((String) x.getKey()).trim();									//得到产生式的左部
-			vset.add(left);																//将左部文法符号加入文法符号集合
-			String[] ss = ((String) x.getValue()).split("\\|");							//用|提取每一右部
+			String left = ((String) x.getKey()).trim();									// 得到产生式的左部
+			vset.add(left);																// 将左部文法符号加入文法符号集合
+			String[] ss = ((String) x.getValue()).split("\\|");							// 用|提取每一右部
 			for(int i = 0;i < ss.length; i++){
 				Production p = new Production();
 				String[] str = ss[i].split("@");	
-				String[] arr = str[0].trim().split(" *, *");							//提取右部所有的文法符号											//提取每一右部的执行动作的方法的路径
-				if(arr.length == 1 && arr[0].equals("EPSLON")){
+				String[] arr = str[0].trim().split(" *, *");							// 提取右部所有的文法符号											//  提取每一右部的执行动作的方法的路径
+				if(arr.length == 1 && arr[0].equals(EPSLON)){
 					p.setRight(new String[0]);
 				} else {
 					p.setRight(arr);
 				}
-				vset.addAll(Arrays.asList(p.getRight()));				//加入文法符号集合
-				p.setAction(str[1].trim());												//设置产生式的动作路径
-				p.setLeft(left);														//设置产生式的左部
+				vset.addAll(Arrays.asList(p.getRight()));								// 加入文法符号集合
+				p.setAction(str[1].trim());												// 设置产生式的动作路径
+				p.setLeft(left);														// 设置产生式的左部
 				vnset.add(left);
 				xset.add(p);								
 			 }
 			 map.put(left, xset);
-			// vset.remove("EPSLON");
+			//   vset.remove(EPSLON);
 		}
-		//System.out.println(map.toString());
+		//  System.out.println(map.toString());
 		Set<Item> sItems = getItemSet(map);
 		System.out.println(sItems.toString());
 		System.out.println( "文法符号:\n" + vset.toString());
@@ -61,7 +62,8 @@ public class GrammaAnalyser {
 		Map<Group, Integer> mGotos = automat(sItems);
 		System.out.println("GOTO表:\n" +  mGotos.toString());
 		
-		System.out.println(getFirstSet(map).toString());
+		System.out.println("FIRST SET:" + getFirstSet(map).toString());
+		System.out.println("LAST SET:" + getLastSet(map).toString());
 		
 	}
 	public static void main(String[] args) {
@@ -77,7 +79,7 @@ public class GrammaAnalyser {
 		for(Entry<String, Set<Production>> ex : map.entrySet()){
 			for(Production px : ex.getValue()){
 				int pos = 0;
-				int len = px.getRight().length;											//得到相同左部的右部总数
+				int len = px.getRight().length;											// 得到相同左部的右部总数
 				Item last = null;														
 				while(pos < len + 1){
 					Item ps = new Item();
@@ -88,70 +90,133 @@ public class GrammaAnalyser {
 					if (last != null) {
 						last.setNext(ps);												
 					}
-					last = ps;															//保存上一个项目
+					last = ps;															// 保存上一个项目
 				}
 			}
 		}
-		//System.out.println(psset.toString());
+		//  System.out.println(psset.toString());
 		return psset;
 	}
 	
 	/**
 	 * 获得非终结符的First集
-	 * @param map
+	 * @param pros
 	 * @return firstSet
 	 */
-	public Map<String, Set<String>> getFirstSet(Map<String, Set<Production>> map){
-		Set<Map.Entry<String, Set<Production>>> entries = map.entrySet();
+	public Map<String, Set<String>> getFirstSet(Map<String, Set<Production>> pros){
+		Set<Map.Entry<String, Set<Production>>> entries = pros.entrySet();
 		for(Entry<String, Set<Production> > ex : entries){
 			String vn = ex.getKey();
-			firstSet.put(vn, new HashSet<String>());									//初始化first集
+			firstSet.put(vn, new HashSet<String>());										// 初始化first集
 		}
 		boolean isChanged = true;
-		while(isChanged){																//循环直到所有的first集不再改变
-			isChanged = false;
-			for(Entry<String, Set<Production> > ex : entries){							//遍历所有的产生式
+		while(isChanged){																	// 循环直到所有的first集不再改变
+			isChanged = false;																// 标记first集是否变化
+			for(Entry<String, Set<Production> > ex : entries){								// 遍历所有的产生式
+				String left = ex.getKey();
+				Set<String> currFirstSet = firstSet.get(left);
+				int beginSize = currFirstSet.size();
 				for(Production p : ex.getValue()){
-					String left = ex.getKey();
 					if(p.getRight().length != 0){
 						String tmp = p.getRight()[0];
-						if(vnset.contains(tmp)){
-							if(firstSet.get(tmp).contains("EPSLON")){
-								firstSet.get(left).addAll(firstSet.get(tmp));
+						if(vnset.contains(tmp)){											// 产生式右部第一个符号是非终结符
+							currFirstSet.addAll(firstSet.get(tmp));							// 将此非终结符的first集加入当前的first集
+							if(firstSet.get(tmp).contains(EPSLON)){							// 非终结符的first集包含EPSLON
 								int len = p.getRight().length;
 								int i = 1;
-								while( i < len &&
-										(firstSet.get(p.getRight()[i - 1]).contains("EPSLON"))){
-									if( vnset.contains(p.getRight()[i])){
-										isChanged = firstSet.get(left).addAll(firstSet.get(p.getRight()[i])) || isChanged;
+								//   判断其后的符号是否为非终结符且其first集是否含有EPSLON
+								while(i < len &&
+										firstSet.get(p.getRight()[i - 1]).contains(EPSLON)){
+									if(vnset.contains(p.getRight()[i])){					// 是非终结符
+										currFirstSet.addAll(firstSet.get(p.getRight()[i]));	// 将其first集加入
 										i++;
 									} else {
-										isChanged = firstSet.get(left).add(p.getRight()[i]) || isChanged;
+										currFirstSet.add(p.getRight()[i]);					// 将终结符加入
 										break;
 									}
 								}
-								if(i == len - 1 && (firstSet.get(p.getRight()[i]).contains("EPSLON"))){
-									isChanged = firstSet.get(left).add("EPSLON") || isChanged;
+								if(i == len													// 若之前所有符号均为非终结符
+									&& (firstSet.get(p.getRight()[i - 1]).contains(EPSLON))){// 最后一个符号其first集包含EPSLON
+									currFirstSet.add(EPSLON);								// 将EPSLON加入当前first集
+								} else {
+									currFirstSet.remove(EPSLON);							// 否则从first集中去除EPSLON
 								}
-							} else {
-								isChanged = firstSet.get(left).addAll(firstSet.get(tmp))|| isChanged;
 							}
 						} else {
-							isChanged =  firstSet.get(left).add(tmp) || isChanged;
+							currFirstSet.add(tmp);
 						}
 					} else {
-						isChanged = firstSet.get(left).add("EPSLON") || isChanged;
+						currFirstSet.add(EPSLON);
+					}
+				}
+				isChanged = isChanged || currFirstSet.size() > beginSize;
+			}
+		}
+		return firstSet;
+	}
+	
+	/**
+	 * 获得非终结符的last集
+	 * @param map
+	 * @return lastSet
+	 */
+	public Map<String, Set<String>> getLastSet(Map<String, Set<Production>> map){
+		Set<Map.Entry<String, Set<Production>>> entries = map.entrySet();
+		for(Entry<String, Set<Production>> ex : entries){
+			String vn = ex.getKey();
+			lastSet.put(vn, new HashSet<String>());												// 初始化last集
+			if (vn.equals(START)){
+				lastSet.get(vn).add(TERMINATOR);
+			}
+		}
+		boolean isChanged = true;
+		while(isChanged){
+			isChanged = false;																	//标记last集是否变化
+			for(Entry<String, Set<Production>> ex : entries){
+				String left = ex.getKey();
+				for(Production p : ex.getValue()){
+					String[] right = p.getRight();
+					for(int i = 0; i < right.length; i++){
+						String tmp = right[i];													// 取出产生式右部第i个符号
+						if(vnset.contains(tmp)){	
+							Set<String> last = lastSet.get(tmp);
+							int lastsize = last.size();
+							if(i < right.length - 1){											// 若为非终结符且不是右部最后一个符号
+								if(!vnset.contains(right[i + 1])){								// 若下一个符号是终结符
+									last.add(right[i + 1]);										// 将其加入非终结符的last集
+								} else {
+									last.addAll(firstSet.get(right[i + 1]));					// 否则加入下一个非终结符的first集
+									last.remove(EPSLON);										// 并去除epslon
+									int x = i + 1; 
+									while(x < right.length - 1){								// 判断之后是否还有非终结符
+										if(vnset.contains(right[x]) 							
+											&& firstSet.get(right[x]).contains(EPSLON)){		// 若有且first集包含EPSLON
+											if(vnset.contains(right[x + 1])){					// 将其first集加入当前的last集
+												last.addAll(firstSet.get(right[x + 1]));
+												x ++;
+											} else {
+												last.add(right[x]);								// 否则将终结符加入当前的last集
+												last.remove(EPSLON);							// 并去除epslon
+												break;
+											}
+										}
+									}
+									if(x == right.length - 1 && vnset.contains(right[x])){
+										last.addAll(lastSet.get(left));
+										last.remove(EPSLON);
+									}
+								}
+							}
+							if(vnset.contains(tmp) && i == right.length - 1){
+								last.addAll(lastSet.get(left));
+							}
+							isChanged = isChanged || last.size() > lastsize;
+						}
 					}
 				}
 			}
 		}
-		
-		return firstSet;
-	}
-	
-	public Map<String, Set<String>> getLastSet(Map<String, Set<Production>> map){
-		Set<Map.Entry<String, Set<Production>>> entries = map.entrySet();
-		return lastSet;
+			return lastSet;
 	}
 
 	/**
@@ -161,42 +226,25 @@ public class GrammaAnalyser {
 	 */
 	public Map<Group, Integer> automat(Set<Item> psset){
 		
-		Set<Item> begin = new HashSet<Item>();											//初始项目集合
+		Set<Item> begin = new HashSet<Item>();											// 初始项目集合
 		for(Item psx : psset){
-			if(psx.getPd().getLeft().equals("start") && psx.getDotPos() == 0){
-				//Set<Item> addItems = new HashSet<Item>();
-				//addItems.add(psx);
-				//while(begin.addAll(addItems)){
-				//	addItems.addAll(getClosure(psset, begin));
-				//}
+			if(psx.getPd().getLeft().equals(START) && psx.getDotPos() == 0){
 				begin.add(psx);
 			}
 		}
 		getClosure(begin);
 		itemfamily.add(begin);
-		//System.out.println("begin: " +begin);
-		//for(int i = 0 ; i < itemfamily.size(); i++){
-		//	int from = i;
-		//}
 		for(int i = 0; i < itemfamily.size(); i++){
-			//System.out.println( " index :" + i + ", itemset: " + itemfamily.get(i).toString());
-			for(String str : vset){														//对每一个文法符号进行GOTO动作
-				Set<Item> sx = getItemFamily(itemfamily.get(i), str);					//得到下一个状态的项目集
+			//  System.out.println( " index :" + i + ", itemset: " + itemfamily.get(i).toString());
+			for(String str : vset){														// 对每一个文法符号进行GOTO动作
+				Set<Item> sx = getItemFamily(itemfamily.get(i), str);					// 得到下一个状态的项目集
 				if(sx.size() == 0) continue;											
-					/*Set<Item> tmp = new HashSet<Item>();
-					boolean isChanged = true;
-					while(isChanged){
-						if(!tmp.addAll(getClosure(sx))){
-							isChanged = false;
-						}
-						sx.addAll(tmp);
-					}*/
-				getClosure(sx);															//求该项目集的闭包
+				getClosure(sx);															// 求该项目集的闭包
 				int to = getKey(itemfamily, sx);
-				if(to <= -1){															//该项目集不存在则创建并产生GOTO转换
+				if(to <= -1){															// 该项目集不存在则创建并产生GOTO转换
 					gotomap.put(new Group(i, str), itemfamily.size());
 					itemfamily.add(sx);
-				} else {																//否则只产生GOTO转换不创建
+				} else {																// 否则只产生GOTO转换不创建
 					gotomap.put(new Group(i, str), to);
 				}
 			}
@@ -215,8 +263,8 @@ public class GrammaAnalyser {
 		Set<Item> set = new HashSet<Item>();
 		for(Item ix : j){
 			if((ix.getDotPos() < (ix.getPd().getRight().length))
-					&& (ix.getPd().getRight()[ix.getDotPos()].equals(prefix))){			//项目未到达末尾且匹配活前缀
-				set.add(ix.getNext());													//将其指向的下一个项目加入集合
+					&& (ix.getPd().getRight()[ix.getDotPos()].equals(prefix))){			// 项目未到达末尾且匹配活前缀
+				set.add(ix.getNext());													// 将其指向的下一个项目加入集合
 			}
 		}
 		return set;
@@ -229,21 +277,21 @@ public class GrammaAnalyser {
 	 */
 	public Set<Item> getClosure(Set<Item> j){
 		int size = 0;
-		Set<Item> tmp = new HashSet<Item>(j);											//临时变量
+		Set<Item> tmp = new HashSet<Item>(j);											// 临时变量
 		while(size < j.size()) {
 			size = j.size();
 			for(Item ix : j){
 				String prefix = null;
 				if(ix.getDotPos() < ix.getPd().getRight().length){			
-					prefix = ix.getPd().getRight()[ix.getDotPos()];						//得到活前缀
+					prefix = ix.getPd().getRight()[ix.getDotPos()];						// 得到活前缀
 				}
 				for(Item xx : psset){
-					if(xx.getPd().getLeft().equals(prefix) && xx.getDotPos() == 0){		//匹配左部等
+					if(xx.getPd().getLeft().equals(prefix) && xx.getDotPos() == 0){		// 匹配左部等
 						tmp.add(xx);
 					}
 				}
 			}
-			Set<Item> change = j;														//交换j和tmp
+			Set<Item> change = j;														// 交换j和tmp
 			j = tmp;
 			tmp = change;
 		}
@@ -257,8 +305,8 @@ public class GrammaAnalyser {
 	 * @return key
 	 */
 	private int getKey(List<Set<Item> > itemfamily, Set<Item> si){
-		int key = -1;																	//不存在则返回-1
-		for( int i = 0; i < itemfamily.size(); i++){									//扫描项目集列表
+		int key = -1;																	// 不存在则返回-1
+		for( int i = 0; i < itemfamily.size(); i++){									// 扫描项目集列表
 			if(itemfamily.get(i).equals(si)){
 				key = i;
 			}
