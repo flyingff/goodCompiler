d2e2593f7b7f85801546d62269675810a73002b5
flyingff <ff@flyingff.net>
2015-07-17 10:53:54 +0800

    新增语义动作类GlobalAction...

---
+++ GoodCompiler/src/test/syntax/action/GlobalAction.java
@@ -0,0 +1,13 @@
+package test.syntax.action;
+
+import syntax.V;
+import syntax.action.SemanticAction;
+
+public class GlobalAction extends SemanticAction {
+	public void a1(V left, V[] right) {
+		System.out.println("A new 语句组;");
+	}
+	public void a2(V left, V[] right) {
+		System.out.println("A 语句 appended;");
+	}
+}
